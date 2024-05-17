d140c4ae585bb174686f9529669acd33edff73ab
Muhammad <muhammadofc07@gmail.com>
2024-05-17 10:26:20 +0700

    lagi dan lagi css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$title}}</title>
-    <link rel="stylesheet" href="{{URL::asset("/css/main.css")}}">
+    <link rel="stylesheet" href={{URL::asset("/css/main.css")}}>
     <link rel="shortcut icon" href="/img/{{$favicon}}" type="image/x-icon">
 </head>
 <body>
