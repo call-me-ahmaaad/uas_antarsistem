e4c8286dbc6aaa5b14636d8fcdc290447c859e0d
Muhammad <muhammadofc07@gmail.com>
2024-05-17 15:41:45 +0700

    Bro

--- resources/views/partials/navbar_tim6.blade.php
+++ resources/views/partials/navbar_tim6.blade.php
@@ -1,3 +1,3 @@
-<a class="navbar-button" href="/figureList">Figure List</a>
-<a class="navbar-button" href="/addFigure">Add Figure</a>
+<a class="navbar-button" href={{route('figure.list')}}>Figure List</a>
+<a class="navbar-button" href={{route('figure.add')}}>Add Figure</a>
 <a class="navbar-button" href="/">Dashboard</a>
