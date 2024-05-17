be4b6624d03dc4cb1fe68ab1de76b473a6b1973f
Muhammad <muhammadofc07@gmail.com>
2024-05-17 10:36:38 +0700

    back again

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WOY</title>
-    {{-- <link rel="stylesheet" href={{asset("/css/main.css")}}> --}}
-    {{-- <link rel="shortcut icon" href="/img/{{$favicon}}" type="image/x-icon"> --}}
+    <title>{{$title}}</title>
+    <link rel="stylesheet" href={{URL::asset("/css/main.css")}}>
+    <link rel="shortcut icon" href="/img/{{$favicon}}" type="image/x-icon">
 </head>
 <body>
     <div class="title">
-        <h1>WOY</h1>
+        <h1>{{$title}}</h1>
     </div>
     <div class="navbar">
         @yield('navbar')
