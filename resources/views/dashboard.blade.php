f6ddf47105ca9abf24e577ee5929730d0a711f50
Muhammad <muhammadofc07@gmail.com>
2024-05-17 10:31:44 +0700

    wa

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Anishop</title>
-    <link rel="stylesheet" href={{URL::asset("/css/dashboard.css")}}>
+    <link rel="stylesheet" href={{asset("/css/dashboard.css")}}>
     <link rel="shortcut icon" href="/img/One Piece.png" type="image/x-icon">
 </head>
 <body>
