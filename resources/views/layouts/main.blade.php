2d4ade1d826df00544ae210977b88901ab591839
Muhammad <muhammadofc07@gmail.com>
2024-05-17 10:32:30 +0700

    aaa

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>WOY</title>
-    <link rel="stylesheet" href={{asset("/css/main.css")}}>
+    {{-- <link rel="stylesheet" href={{asset("/css/main.css")}}> --}}
     {{-- <link rel="shortcut icon" href="/img/{{$favicon}}" type="image/x-icon"> --}}
 </head>
 <body>
