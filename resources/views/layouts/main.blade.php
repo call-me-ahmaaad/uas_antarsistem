2c350570cade2a628da038505460cef107c3f0c7
Muhammad <muhammadofc07@gmail.com>
2024-05-17 10:30:25 +0700

    TEST

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$title}}</title>
+    <title>WOY</title>
     <link rel="stylesheet" href={{URL::asset("/css/main.css")}}>
     {{-- <link rel="shortcut icon" href="/img/{{$favicon}}" type="image/x-icon"> --}}
 </head>
 <body>
     <div class="title">
-        <h1>{{$title}}</h1>
+        <h1>WOY</h1>
     </div>
     <div class="navbar">
         @yield('navbar')
