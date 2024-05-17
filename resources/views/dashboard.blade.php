b392391b8a939e4bae969b8ef30bfd715ea5e194
Muhammad <muhammadofc07@gmail.com>
2024-05-17 10:49:56 +0700

    aw

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,7 +16,7 @@
 
     {{-- Container: Navigation Card Button --}}
     <div class="container">
-        <a class="container card" href={{asset("/figureList")}}>
+        <a class="container card" href="/figureList">
             <h3>Figure List</h3>
             <p>Menampilkan daftar action figure di inventori.</p>
         </a>
